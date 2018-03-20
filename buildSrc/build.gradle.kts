67f1f7bb37de16d1d5c567296025f0d6a3af8ca5
Paul Merlin <paul@gradle.com>
2018-03-20 16:15:53 +0100

    No need to use the<T>()

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -84,7 +84,7 @@ subprojects {
             tasks.create<ValidateTaskProperties>("validateTaskProperties") {
                 outputFile.set(project.the<ReportingExtension>().baseDirectory.file("task-properties/report.txt"))
 
-                val mainSourceSet = project.the<JavaPluginConvention>().sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
+                val mainSourceSet = project.java.sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
                 classes = mainSourceSet.output.classesDirs
                 classpath = mainSourceSet.compileClasspath
                 dependsOn(mainSourceSet.output)
